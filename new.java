4d79f27112360354242eabdd455006a9f78778c8
Davido-amos <65489499+Davido-amos@users.noreply.github.com>
2020-06-18 15:08:43 +0200

    javasample

---
+++ new.java
@@ -0,0 +1,12 @@
+public class apples{
+    
+      static int number;
+      
+      public static void main(String[] args){
+      
+      System.out.println("Welcome");
+      
+        }
+      
+      }
+   
